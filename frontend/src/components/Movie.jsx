5786dd0740681c44fbdbb0c4046637d2bf311233
Lionel Manzetti <lionel.manzetti@gmail.com>
2022-04-20 14:54:37 +0200

    Fixed the accessibility

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 function Movie({ movie }) {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full"
+    <div className="w-1/2 rounded overflow-hidden shadow-lg ml-4 mr-4">
+      <img
+        className="max-h-40 h-full w-full object-cover object-center"
         src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
         alt="movie"
       />
-    <div className="px-6 py-4">
-      <h3 className="font-bold text-xl mb-2">{movie.title}</h3>
-      <p className="text-gray-700 text-base">{movie.release_date.slice(0, 4)}</p>
-    </div>
-      
+      <div className="px-2 py-3 h-20">
+        <h3 className="font-bold text-base mb-2 truncate">{movie.title}</h3>
+        <p className="text-gray-700 text-base">
+          {movie.release_date.slice(0, 4)}
+        </p>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,29 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import logo from "../assets/logo.png";
+import logo from "../assets/alt-logo.png";
 import Movie from "../components/Movie";
+import dice from "../assets/dice.png";
 
 function Settings() {
-
   const [movieStart, setMovieStart] = useState();
   const [movieEnd, setMovieEnd] = useState();
-
-  const API_KEY = process.env.REACT_APP_TMDB_KEY;
 
   const randomMovieId = [];
 
   const random = (index) => {
-    randomMovieId[index] = Math.floor(Math.random() * (900 - 100) + 100);
-    console.log(randomMovieId)
+    randomMovieId[index] = Math.floor(Math.random() * (400 - 100) + 100);
     return randomMovieId[index];
   };
 
   const generateMovies = (callback, index) => {
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/${random(
-          index
-        )}?api_key=process.env.REACT_APP_TMDB_KEY&language=fr`
+        `https://api.themoviedb.org/3/movie/${random(index)}?api_key=${
+          import.meta.env.VITE_API_KEY
+        }&language=fr`
       )
       .then((res) => {
         callback(res.data);
@@ -31,22 +28,36 @@ function Settings() {
   };
 
   useEffect(() => {
-    generateMovies(setMovieStart, 0)
-    generateMovies(setMovieEnd, 1)
-  }, [])
+    generateMovies(setMovieStart, 0);
+    generateMovies(setMovieEnd, 1);
+    if (movieStart === movieEnd) {
+      generateMovies(setMovieEnd, 1);
+    }
+  }, []);
 
   return (
-    <div>
-      <img src={logo} alt="logo" />
-      <div>
-        {movieStart ? <Movie movie={movieStart} /> : ""}
-        {movieEnd ? <Movie movie={movieEnd} /> : ""}
-        <button type="button" onClick={() => {
-          generateMovies(setMovieStart, 0)
-          generateMovies(setMovieEnd, 1)}}>
-          Change Movies
-        </button>
+    <div className="flex flex-col">
+      <img
+        className="w-6/12 aspect-square self-center"
+        src={logo}
+        alt="alt-logo"
+      />
+      <div className="flex flex-col">
+        <div className="flex flex-row">
+          {movieStart ? <Movie movie={movieStart} /> : ""}
+          {movieEnd ? <Movie movie={movieEnd} /> : ""}
+        </div>
       </div>
+      <button
+        className="self-center m-2"
+        type="submit"
+        onClick={() => {
+          generateMovies(setMovieStart, 0);
+          generateMovies(setMovieEnd, 1);
+        }}
+      >
+        <img className="h-12" src={dice} alt="dice" />
+      </button>
     </div>
   );
 }
