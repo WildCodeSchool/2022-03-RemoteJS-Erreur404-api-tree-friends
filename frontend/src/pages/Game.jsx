3268dfb848aac3e64ecf0afbb44cbd641922eda6
ETienne <etiennebacleapi@gmail.com>
2022-04-22 10:07:18 +0200

    rajout des minutes sur le chrono de la page game

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -2,14 +2,22 @@ import React, { useEffect, useState } from "react";
 
 function Game() {
   const [time, setTime] = useState(0);
+  const [minutes, setMinutes] = useState(0);
   useEffect(() => {
     setTimeout(() => {
-      setTime(time + 1);
+      if (time < 59) {
+        setTime(time + 1);
+      } else {
+        setTime(0);
+        setMinutes(minutes + 1);
+      }
     }, 1000);
   }, [time]);
   return (
     <div className="flex flex-col">
-      <h1 className="self-center mt-4 font-bold text-xl">{time} s</h1>
+      <h1 className="self-center mt-4 font-bold text-xl">
+        {minutes}m {time}s
+      </h1>
     </div>
   );
 }
