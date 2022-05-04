8782542cc12cd5e265143682607e90f036d3a872
Lionel Manzetti <lionel.manzetti@gmail.com>
2022-05-04 12:57:52 +0200

    Set the gameplay and cleaned the Game page

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -10,64 +10,44 @@ import LogoLink from "../components/LogoLink";
 import ExportContext from "../contexts/MovieContext";
 import Chrono from "../components/Chrono";
 
-import fightClub from "../data/fightClub";
 import logo from "../assets/alt-logo.png";
 
 function Game() {
   const { moviesId } = useContext(ExportContext.MovieContext);
-  const [position, setPosition] = useState(fightClub);
-  const [destination, setDestination] = useState(fightClub);
-  const [carousel, setCarousel] = useState(false);
+  const [position, setPosition] = useState("");
+  const [destination, setDestination] = useState("");
+  const [carousel, setCarousel] = useState("");
   const [openRules, setOpenRules] = useState(false);
   const [homeLink, setHomeLink] = useState(false);
+  const [type, setType] = useState("movie");
+  const [carouselType, setCarouselType] = useState("credits");
+
+  window.onbeforeunload = () => {
+    return "I don't really know how this works yet";
+  };
 
   const switchCarousel = (id) => {
-    if (position.title) {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${
-            import.meta.env.VITE_API_KEY
-          }&language=fr`
-        )
-        .then((res) => {
-          setCarousel(res.data);
-        });
-    } else {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${
-            import.meta.env.VITE_API_KEY
-          }&language=fr`
-        )
-        .then((res) => {
-          setCarousel(res.data);
-        });
-    }
+    axios
+      .get(
+        `https://api.themoviedb.org/3/${type}/${id}/${carouselType}?api_key=${
+          import.meta.env.VITE_API_KEY
+        }&language=fr`
+      )
+      .then((res) => {
+        setCarousel(res.data);
+      });
   };
 
   const switchPosition = (id) => {
-    if (position.title) {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/movie/${id}?api_key=${
-            import.meta.env.VITE_API_KEY
-          }&language=fr`
-        )
-        .then((res) => {
-          setPosition(res.data);
-        });
-    } else {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/person/${id}?api_key=${
-            import.meta.env.VITE_API_KEY
-          }&language=fr`
-        )
-        .then((res) => {
-          setPosition(res.data);
-        });
-    }
-    switchCarousel(position.id);
+    axios
+      .get(
+        `https://api.themoviedb.org/3/${type}/${id}?api_key=${
+          import.meta.env.VITE_API_KEY
+        }&language=fr`
+      )
+      .then((res) => {
+        setPosition(res.data);
+      });
   };
 
   const createDestination = (id) => {
@@ -82,17 +62,34 @@ function Game() {
       });
   };
 
+  const changeData = (id) => {
+    switchPosition(id);
+  };
+
   useEffect(() => {
-    switchPosition(moviesId[0]);
+    changeData(moviesId[0]);
     createDestination(moviesId[1]);
   }, []);
+
+  useEffect(() => {
+    switchCarousel(position.id);
+    setTimeout(() => {
+      if (type === "movie") {
+        setType("person");
+        setCarouselType("movie_credits");
+      } else {
+        setType("movie");
+        setCarouselType("credits");
+      }
+    }, 200);
+  }, [position.id]);
 
   return (
     <div>
       <div className="flex flex-col">
         <Chrono />
 
-        <Position place={position} />
+        {position !== "" && <Position place={position} />}
         <div
           style={{
             maxWidth: 350,
@@ -101,14 +98,16 @@ function Game() {
             marginRight: "auto",
           }}
         >
-          {carousel ? (
+          {carousel !== "" && (
             <Carousel>
               {carousel.cast.slice(0, 10).map((e) => (
-                <CarouselElement element={e} key={e.id} />
+                <CarouselElement
+                  element={e}
+                  key={e.id}
+                  changeData={changeData}
+                />
               ))}
             </Carousel>
-          ) : (
-            <div />
           )}
         </div>
         <div className="m-12 mt-0 mb-6 h-40">
